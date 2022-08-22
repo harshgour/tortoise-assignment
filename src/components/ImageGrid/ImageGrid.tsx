fa675bd2f7fc26c841110535cfaac1de888d37f3
Harsh Gour <harshgour57@gmail.com>
2022-08-22 14:11:38 +0530

    bug: (conditional image-grid loading)

--- src/components/ImageGrid/ImageGrid.tsx
+++ src/components/ImageGrid/ImageGrid.tsx
@@ -43,74 +43,72 @@ const ImageGrid: React.FC<ImageGridProps> = (props) => {
 
 	return (
 		<>
-			<ConditionalComponent isVisible={!loading}>
-				<div className='image-container container mx-auto flex flex-wrap md:px-2 mt-12 row'>
-					{allImages.map((columnImages, index) => {
-						return (
-							<div
-								className='column md:px-2 flex-[100%] md:flex-[50%] lg:flex-[33.33%]'
-								key={index}>
-								{columnImages.map(({ image, user }, imageIndex) => {
-									return (
-										<div
-											className={`images relative mt-14 md:mt-4`}
-											key={imageIndex}>
-											<ImageBackDrop
-												user={user}
-												image={image}
-												handleImageDownload={handleImageDownload}
-												handleImageLike={handleImageLike}
-											/>
-											<ImageWrap
-												user={user}
-												image={image}
-												handleImageDownload={handleImageDownload}>
-												{index + 1 === allImages.length &&
-												imageIndex + 1 === columnImages.length - 1 ? (
-													<div
-														style={{
-															backgroundColor: image["color"],
-															minHeight: `${Math.floor(
-																image["aspect_ratio"] * 320,
-															)}px`,
-														}}>
-														<img
-															key={imageIndex}
-															src={image["url"]}
-															alt=''
-															className='w-full align-middle hover:brightness-[0.8] cursor-pointer'
-															loading='eager'
-															ref={lastImageRef}
-															sizes='(min-width: 1335px) 416px, (min-width: 992px) calc(calc(100vw - 72px) / 3), (min-width: 768px) calc(calc(100vw - 48px) / 2), 100vw'
-														/>
-													</div>
-												) : (
-													<div
-														style={{
-															backgroundColor: image["color"],
-															minHeight: `${Math.floor(
-																image["aspect_ratio"] * 320,
-															)}px`,
-														}}>
-														<img
-															key={imageIndex}
-															src={image["url"]}
-															alt=''
-															className='w-full align-middle hover:brightness-[0.8] cursor-pointer'
-															loading='eager'
-															sizes='(min-width: 1335px) 416px, (min-width: 992px) calc(calc(100vw - 72px) / 3), (min-width: 768px) calc(calc(100vw - 48px) / 2), 100vw'
-														/>
-													</div>
-												)}
-											</ImageWrap>
-										</div>
-									);
-								})}
-							</div>
-						);
-					})}
-				</div>
-			</ConditionalComponent>
+			<div className='image-container container mx-auto flex flex-wrap md:px-2 mt-12 row'>
+				{allImages.map((columnImages, index) => {
+					return (
+						<div
+							className='column md:px-2 flex-[100%] md:flex-[50%] lg:flex-[33.33%]'
+							key={index}>
+							{columnImages.map(({ image, user }, imageIndex) => {
+								return (
+									<div
+										className={`images relative mt-14 md:mt-4`}
+										key={imageIndex}>
+										<ImageBackDrop
+											user={user}
+											image={image}
+											handleImageDownload={handleImageDownload}
+											handleImageLike={handleImageLike}
+										/>
+										<ImageWrap
+											user={user}
+											image={image}
+											handleImageDownload={handleImageDownload}>
+											{index + 1 === allImages.length &&
+											imageIndex + 1 === columnImages.length - 1 ? (
+												<div
+													style={{
+														backgroundColor: image["color"],
+														minHeight: `${Math.floor(
+															image["aspect_ratio"] * 320,
+														)}px`,
+													}}>
+													<img
+														key={imageIndex}
+														src={image["url"]}
+														alt=''
+														className='w-full align-middle hover:brightness-[0.8] cursor-pointer'
+														loading='eager'
+														ref={lastImageRef}
+														sizes='(min-width: 1335px) 416px, (min-width: 992px) calc(calc(100vw - 72px) / 3), (min-width: 768px) calc(calc(100vw - 48px) / 2), 100vw'
+													/>
+												</div>
+											) : (
+												<div
+													style={{
+														backgroundColor: image["color"],
+														minHeight: `${Math.floor(
+															image["aspect_ratio"] * 320,
+														)}px`,
+													}}>
+													<img
+														key={imageIndex}
+														src={image["url"]}
+														alt=''
+														className='w-full align-middle hover:brightness-[0.8] cursor-pointer'
+														loading='eager'
+														sizes='(min-width: 1335px) 416px, (min-width: 992px) calc(calc(100vw - 72px) / 3), (min-width: 768px) calc(calc(100vw - 48px) / 2), 100vw'
+													/>
+												</div>
+											)}
+										</ImageWrap>
+									</div>
+								);
+							})}
+						</div>
+					);
+				})}
+			</div>
 			<ConditionalComponent isVisible={loading}>
 				<div className='text-center'>
 					<Loader />
